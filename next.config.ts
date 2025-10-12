a3e9429d31e8c09af06ab54c9826a8f799b902ed
Simeon Dodov <s.dodov@outlook.com>
2025-10-12 04:15:53 +0300

    test

--- next.config.ts
+++ next.config.ts
@@ -7,21 +7,6 @@ const nextConfig: NextConfig = {
       allowedOrigins: ["*"],
     },
   },
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.resolve.alias = {
-        ...(config.resolve.alias || {}),
-        "bluetooth-hci-socket": "@abandonware/bluetooth-hci-socket",
-      };
-      config.externals.push({
-        "@abandonware/bluetooth-hci-socket":
-          "commonjs @abandonware/bluetooth-hci-socket",
-        "noble-mac": "commonjs noble-mac",
-        "xpc-connection": "commonjs xpc-connection",
-      });
-    }
-    return config;
-  },
 };
 
 export default nextConfig;
