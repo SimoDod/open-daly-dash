0db5c9a5aa3ef967e435b3ad1887f880e051dba3
Simeon Dodov <s.dodov@outlook.com>
2026-01-22 16:15:07 +0200

    move variable to env

--- lib/bms/service.ts
+++ lib/bms/service.ts
@@ -80,7 +80,7 @@ class BmsService extends EventEmitter {
 
         // --- Pushcut notification logic ---
         if (typeof state.soc_pct === "number") {
-          const LOW_SOC = 30;
+          const LOW_SOC = Number(process.env.SOC_PERCENTAGE_NOTIFICATION_TRIGGER);
           if (state.soc_pct < LOW_SOC && !state._lowSocNotified) {
             fetch(process.env.PUSH_NOTIFICATION_URL!, { method: "GET" }).catch(
               () => {},
